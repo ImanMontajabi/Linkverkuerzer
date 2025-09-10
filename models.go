e8c99d0bde1b1cebc2a515778b1cd18010d16eac
Iman Montajabi <iman.montajabi@gmail.com>
2025-09-10 03:39:24 +0200

    Add models.go

---
+++ models.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type URL struct {
+	ID          uint           `json:"id" gorm:"primarykey"`
+	OriginalURL string         `json:"original_url" gorm:"not null"`
+	ShortCode   string         `json:"short_code" gorm:"unique;not null;index"`
+	ClickCount  int            `json:"click_count" gorm:"default:0"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
+
+type ShortenRequest struct {
+	URL string `json:"url" validate:"required,url"`
+}
+
+type ShortenResponse struct {
+	OriginalURL string `json:"original_url"`
+	ShortURL    string `json:"short_url"`
+	ShortCode   string `json:"short_code"`
+}
+
+type StatsResponse struct {
+	ShortCode   string    `json:"short_code"`
+	OriginalURL string    `json:"original_url"`
+	ClickCount  int       `json:"click_count"`
+	CreatedAt   time.Time `json:"created_at"`
+}
